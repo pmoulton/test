1b432aad1989ba4e82e96c308b95da5b61725a3c
Paul Moulton <pmoulton2@gmail.com>
2015-02-21 15:10:59 -0800

    Backend is connected to smartthings

--- watchcode/watchdog/views.py
+++ watchcode/watchdog/views.py
@@ -8,11 +8,12 @@ def check_activity(request):
     if request.method == "GET":
         return HttpResponse(status=400)
     username = request.POST['username']
-    # print username
     time = util.get_event_time(username, 0)
     if time == None:
         return HttpResponse(status=403)
-    # print time
     bool = not util.greater_than(time)
-    response_data = {'error': "none", 'start': bool}
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
+    if bool:
+    	return HttpResponse(status=200)
+    return HttpResponse(status=202)
+    # response_data = {'error': "none", 'start': bool}
+    # return HttpResponse(json.dumps(response_data), content_type="application/json")
